923029bae48a6429a6a10ec836ee9b17ade22e4e
amandag0mes <32399201+amandag0mes@users.noreply.github.com>
2019-12-06 02:21:58 -0500

    minor route change

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,9 @@ const tutorial = require("./tutorial");
 const path = require("path");
 
 const constructorMethod = app =>{
-	app.get("/", (req, res) => {
-		res.sendFile(path.resolve("static/index.html"));
-	});
+    app.use("/", mainscreen);
 	app.use("/achievements", achievements);
     app.use("/quiz", quiz);
-    app.use("/mainscreen", mainscreen);
     app.use("/tutorial.js", tutorial);
 	
 	app.use("*", (req,res) => {
