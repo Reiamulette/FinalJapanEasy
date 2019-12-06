873eb634c753b0544c5f0e16013285d3a9874b50
Reiamulette <yao6488@gmail.com>
2019-12-06 05:33:59 -0500

    Fixed Game; Added Questions
    It's still a rather small database of questions (55 I think). But you should be able to figure out a, i, and ku in hiragana and katakana form. The game runs on questions.json now.

--- 12. Create a Spinning Loader/game.js
+++ 12. Create a Spinning Loader/game.js
@@ -13,34 +13,13 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch(
-  "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
-)
+fetch("questions.json")
   .then(res => {
     return res.json();
   })
   .then(loadedQuestions => {
-    console.log(loadedQuestions.results);
-    questions = loadedQuestions.results.map(loadedQuestion => {
-      const formattedQuestion = {
-        question: loadedQuestion.question
-      };
-
-      const answerChoices = [...loadedQuestion.incorrect_answers];
-      formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
-      answerChoices.splice(
-        formattedQuestion.answer - 1,
-        0,
-        loadedQuestion.correct_answer
-      );
-
-      answerChoices.forEach((choice, index) => {
-        formattedQuestion["choice" + (index + 1)] = choice;
-      });
-
-      return formattedQuestion;
-    });
-
+    console.log(loadedQuestions);
+    questions = loadedQuestions;
     startGame();
   })
   .catch(err => {
@@ -49,7 +28,7 @@ fetch(
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 5;
 
 startGame = () => {
   questionCounter = 0;
